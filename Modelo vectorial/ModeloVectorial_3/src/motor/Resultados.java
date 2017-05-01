4752d7c33b35e7b8020613e4207b3a6a4dcdf03f
Rodrigo Borges <Rodrigo Borges>
2017-05-01 18:49:19 +0200

    liadiña

---
+++ Modelo vectorial/ModeloVectorial_3/src/motor/Resultados.java
@@ -0,0 +1,78 @@
+package motor;
+
+import java.util.ArrayList;
+
+public class Resultados {
+	private String r5;
+	private String p5;
+	private String f5;
+	private String r10;
+	private String p10;
+	private String f10;
+	private String reciprocall1;
+	private String reciprocall2;
+	private String average;
+	private ArrayList<String> calcnDCG;
+	public String getR5() {
+		return r5;
+	}
+	public void setR5(String r5) {
+		this.r5 = r5;
+	}
+	public String getP5() {
+		return p5;
+	}
+	public void setP5(String p5) {
+		this.p5 = p5;
+	}
+	public String getF5() {
+		return f5;
+	}
+	public void setF5(String f5) {
+		this.f5 = f5;
+	}
+	public String getR10() {
+		return r10;
+	}
+	public void setR10(String r10) {
+		this.r10 = r10;
+	}
+	public String getP10() {
+		return p10;
+	}
+	public void setP10(String p10) {
+		this.p10 = p10;
+	}
+	public String getF10() {
+		return f10;
+	}
+	public void setF10(String f10) {
+		this.f10 = f10;
+	}
+	public String getReciprocall1() {
+		return reciprocall1;
+	}
+	public void setReciprocall1(String reciprocall1) {
+		this.reciprocall1 = reciprocall1;
+	}
+	public String getReciprocall2() {
+		return reciprocall2;
+	}
+	public void setReciprocall2(String reciprocall2) {
+		this.reciprocall2 = reciprocall2;
+	}
+	public String getAverage() {
+		return average;
+	}
+	public void setAverage(String average) {
+		this.average = average;
+	}
+	public ArrayList<String> getCalcnDCG() {
+		return calcnDCG;
+	}
+	public void setCalcnDCG(ArrayList<String> calcnDCG) {
+		this.calcnDCG = calcnDCG;
+	}
+	
+	
+}
