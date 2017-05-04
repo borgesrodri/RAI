72fafa8c17ba923d39eb88c64a0a666351ab1be3
david del rey <david del rey>
2017-05-04 10:30:22 +0200

    Nuevo stemer

--- Modelo vectorial/MotorRecuperacionAmpliado_3/src/modelo/Indizador.java
+++ Modelo vectorial/MotorRecuperacionAmpliado_3/src/modelo/Indizador.java
@@ -4,11 +4,23 @@ public class Indizador {
 	public Indizador(){
 		
 	}
-	public String  stemTerm (String term) {
+	/*public String  stemTerm (String term) {
 		PorterStemmer stem = new PorterStemmer();
 		stem.setCurrent(term);
 		stem.stem();
 		String result = stem.getCurrent();
         return result;
+	}*/
+	public String stemTerm(String term){
+		if(term.endsWith("ed")){
+			return term.substring(0, term.length()-1);
+		}
+		if(term.endsWith("ing")){
+			return term.substring(0, term.length()-3);
+		}
+		if(term.endsWith("s")){
+			return term.substring(0, term.length()-1);
+		}
+		return term;
 	}
 }
